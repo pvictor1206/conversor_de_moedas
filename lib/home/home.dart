9694175238064935fb4a33cfba09a68658ee2871
pvictor1206 <paulomagalhaes1206@gmail.com>
2021-05-07 19:49:37 -0300

    Aula 02

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,2 +1,26 @@
 import 'package:flutter/material.dart';
 
+class Home extends StatefulWidget {
+  const Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+            "\$ Conversor de Moedas \$",
+          style: TextStyle(color: Colors.black),
+
+        ),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'core/app_widget.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json-cors&key=5fb3949a";
 
-
 void main()  async{
-
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MyApp());
+}
+
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
