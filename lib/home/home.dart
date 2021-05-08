d17745128120165a21f742fe4d40661fb31a28a4
pvictor1206 <paulomagalhaes1206@gmail.com>
2021-05-07 21:53:32 -0300

    Aula 03

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:conversor_de_moedas/main.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -21,6 +22,55 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber,
         centerTitle: true,
       ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                    "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber,
+                  fontSize: 25.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if(snapshot.hasError){
+                return Center(
+                  child: Text(
+                    "ERRO AO CARREGAR OS DADOS",
+                    style: TextStyle(color: Colors.amber,
+                      fontSize: 25.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color: Colors.green,);
+              }
+          }
+        }
+      ),
     );
   }
 }
+
+/*
+
+      theme: ThemeData(
+          hintColor: Colors.amber,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            enabledBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+            focusedBorder:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+            hintStyle: TextStyle(color: Colors.amber),
+          )),
+    ));
+
+    
+ */
